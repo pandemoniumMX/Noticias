0be6424678067a02e5bb2769bec82d360c2f809d
feenandogarcia <45440411+feenandogarcia@users.noreply.github.com>
2018-12-04 11:41:56 -0700

    actualizacion prra

---
+++ acercade.php
@@ -0,0 +1,82 @@
+  <!DOCTYPE html>
+  <html>
+  <head>
+  <meta http-equiv="Content-Type";
+    content-"text/html; charset-utf-8"/>
+    <meta charset="utf-8">
+    <title>Ejemplo con CSS</title>
+    <link rel="stylesheet" type="text/css" href="css/estilos.css" media="screen" />
+        <link rel="stylesheet" type="text/css" href="css/estilos_base.css" media="screen" />
+
+      <div class="header">
+          <div class="conthead"></div>
+          <div class="conthead">
+            <div class="contheadlogo"><img  src="images/logo.png"></div>
+          </div>
+          <div class="conthead"></div>
+      </div>
+    <div class="nav" >
+      <ul>
+        <li ><a href="index.php">INICIO</a></li>
+        <li><a href="catalogo.php">CATALOGO</a></li>
+        <li><a href="catalogo2.php">CATALOGO2</a></li>
+        <li><a href="contacto.php" >CONTACTO</a></li>
+        <li><a href="acercade.php"  class="active">ACERCA DE</a></li>
+
+        </li>
+      </ul>
+    </div>
+
+    </nav>
+  </head>
+
+<body>
+
+
+<div class="contentmv">
+    <div class="mv">
+    <h2>MISION</h2>
+    <P>Satisfacer las necesidades, deseos y expectativas de entretenimiento y esparcimiento en el tiempo libre de las diferentes familias, convirtiéndonos en su mejor opción, así como ser prestadores de servicios en esta empresa, constituyendo un círculo de negocios que genere e impulse el desarrollo socioeconómico de las personas que interactúan con nuestra organización.</P>
+
+    </div>
+
+    <div class="mv">
+    <H2>VISION</H2>
+        <P>Satisfacer las necesidades, deseos y expectativas de entretenimiento y esparcimiento en el tiempo libre de las diferentes familias, convirtiéndonos en su mejor opción, así como ser prestadores de servicios en esta empresa, constituyendo un círculo de negocios que genere e impulse el desarrollo socioeconómico de las personas que interactúan con nuestra organización.</P>
+    </div>
+</div>
+
+<div class="contenteqipo">
+<h2>DISEÑADORES</h2>
+  <div class="integrante"><img  src="images/imagen.png"> <h1>Fernando</h1></div>
+  <div class="integrante"><img  src="images/imagen.png"> <h1>zurisadai</h1></div>
+
+</div>
+
+<div class="contenteqipo">
+<h2>PROGRAMADORES</h2>
+  <div class="integrante"><img  src="images/imagen.png"> <h1>Fernando</h1></div>
+  <div class="integrante"><img  src="images/imagen.png"> <h1>zurisadai</h1></div>
+
+</div>
+
+
+
+</body>
+
+  <footer>
+  <div class="footer">
+    <div class="contfoot"></div>
+    <div class="contfoot">
+      <div class="contfootredes">
+        <h2>SIGUENOS EN:</h2>
+        <img  src="images/facebook.png">
+        <img  src="images/twitter.png">
+        <img  src="images/instagram.png">
+        <img  src="images/whatsapp.png">
+      </div>
+    </div>
+    <div class="contfoot"></div>
+  </div>
+</footer>
+  </html>
--- bd/noticpelic (1).sql
+++ bd/noticpelic (1).sql
@@ -3,7 +3,7 @@
 -- https://www.phpmyadmin.net/
 --
 -- Servidor: 127.0.0.1
--- Tiempo de generación: 28-11-2018 a las 22:28:59
+-- Tiempo de generación: 04-12-2018 a las 18:32:44
 -- Versión del servidor: 5.7.14
 -- Versión de PHP: 5.6.25
 
@@ -26,8 +26,6 @@ SET time_zone = "+00:00";
 -- Estructura de tabla para la tabla `noticias`
 --
 
-use noticpelic;
-
 CREATE TABLE `noticias` (
   `ID_NOTICIA` int(11) NOT NULL,
   `NOT_TITULO` varchar(50) DEFAULT NULL,
@@ -43,13 +41,25 @@ CREATE TABLE `noticias` (
 --
 
 INSERT INTO `noticias` (`ID_NOTICIA`, `NOT_TITULO`, `NOT_SUBTITULO`, `NOT_CONTENIDO`, `NOT_IMG`, `NOT_FECHA`, `NOT_ESTADO`) VALUES
-(1, 'Juego del día', 'El super juego', 'este juego esta relasionado con los cmando d emysql el cual nos brinda los servicios los cuales prorpociona documentos de alta inteligencia', NULL, '2018-11-28 18:49:07', 'activo'),
-(15, 'Juego del día', 'El super juego', 'este juego esta relasionado con los cmando d emysql el cual nos brinda los servicios los cuales prorpociona documentos de alta inteligencia', NULL, '2018-11-28 18:49:38', 'activo'),
-(16, 'galaxy', 'Samnsung Galaxy s9', 'Los Samsung Galaxy S9 y S9+ acaban de ser presentados en el MWC 2018, y ya conocemos sus precios y la fecha en la que aterrizarÃ¡n en las tiendas. Ahora que ya hemos visto todas las caracterÃ­sticas, especificaciones y detalles de los Galaxy S9 y S9+, y lo hemos comparado con uno de los grandes rivales como lo es el iPhone X, llega el momento de ver cuÃ¡ndo y cÃ³mo se podrÃ¡ comprar este dÃºo de terminales estrella surcoreanos, llamados a ser dos de los mÃ¡s vendidos del aÃ±o dentro del segmento del a gama alta.\r\n\r\nSi bien es cierto que el precio ha aumentado con respecto a los anteriores Galaxy S8 y S8+, no es un crecimiento que nos pille por sorpresa, pues la infinidad de filtraciones aparecidas sobre la pareja de telÃ©fonos Galaxy ya apuntaban a un aumento.\r\n\r\nSin mÃ¡s, pasamos a ver cuÃ¡nto costarÃ¡n los Samsung Galaxy S9 y S9+ en EspaÃ±a, LatinoamÃ©rica, y el resto de regiones del mundo en las que Samsung venderÃ¡ de forma oficial sus nuevos terminales.', 'galeria/noticias/noti1.jpg', '2018-11-28 21:45:28', 'publicada'),
 (17, 'galaxy', 'Samnsung Galaxy s9', 'Los Samsung Galaxy S9 y S9+ acaban de ser presentados en el MWC 2018, y ya conocemos sus precios y la fecha en la que aterrizarÃ¡n en las tiendas. Ahora que ya hemos visto todas las caracterÃ­sticas, especificaciones y detalles de los Galaxy S9 y S9+, y lo hemos comparado con uno de los grandes rivales como lo es el iPhone X, llega el momento de ver cuÃ¡ndo y cÃ³mo se podrÃ¡ comprar este dÃºo de terminales estrella surcoreanos, llamados a ser dos de los mÃ¡s vendidos del aÃ±o dentro del segmento del a gama alta.\r\n\r\nSi bien es cierto que el precio ha aumentado con respecto a los anteriores Galaxy S8 y S8+, no es un crecimiento que nos pille por sorpresa, pues la infinidad de filtraciones aparecidas sobre la pareja de telÃ©fonos Galaxy ya apuntaban a un aumento.\r\n\r\nSin mÃ¡s, pasamos a ver cuÃ¡nto costarÃ¡n los Samsung Galaxy S9 y S9+ en EspaÃ±a, LatinoamÃ©rica, y el resto de regiones del mundo en las que Samsung venderÃ¡ de forma oficial sus nuevos terminales.', 'galeria/noticias/noti1.jpg', '2018-11-28 21:45:28', 'publicada'),
-(18, 'galaxy', 'Samnsung Galaxy s9', 'Los Samsung Galaxy S9 y S9+ acaban de ser presentados en el MWC 2018, y ya conocemos sus precios y la fecha en la que aterrizarÃ¡n en las tiendas. Ahora que ya hemos visto todas las caracterÃ­sticas, especificaciones y detalles de los Galaxy S9 y S9+, y lo hemos comparado con uno de los grandes rivales como lo es el iPhone X, llega el momento de ver cuÃ¡ndo y cÃ³mo se podrÃ¡ comprar este dÃºo de terminales estrella surcoreanos, llamados a ser dos de los mÃ¡s vendidos del aÃ±o dentro del segmento del a gama alta.\r\n\r\nSi bien es cierto que el precio ha aumentado con respecto a los anteriores Galaxy S8 y S8+, no es un crecimiento que nos pille por sorpresa, pues la infinidad de filtraciones aparecidas sobre la pareja de telÃ©fonos Galaxy ya apuntaban a un aumento.\r\n\r\nSin mÃ¡s, pasamos a ver cuÃ¡nto costarÃ¡n los Samsung Galaxy S9 y S9+ en EspaÃ±a, LatinoamÃ©rica, y el resto de regiones del mundo en las que Samsung venderÃ¡ de forma oficial sus nuevos terminales.', 'galeria/noticias/noti1.jpg', '2018-11-28 21:45:28', 'publicada'),
-(19, 'galaxy', 'Samnsung Galaxy s9', 'Los Samsung Galaxy S9 y S9+ acaban de ser presentados en el MWC 2018, y ya conocemos sus precios y la fecha en la que aterrizarÃ¡n en las tiendas. Ahora que ya hemos visto todas las caracterÃ­sticas, especificaciones y detalles de los Galaxy S9 y S9+, y lo hemos comparado con uno de los grandes rivales como lo es el iPhone X, llega el momento de ver cuÃ¡ndo y cÃ³mo se podrÃ¡ comprar este dÃºo de terminales estrella surcoreanos, llamados a ser dos de los mÃ¡s vendidos del aÃ±o dentro del segmento del a gama alta.\r\n\r\nSi bien es cierto que el precio ha aumentado con respecto a los anteriores Galaxy S8 y S8+, no es un crecimiento que nos pille por sorpresa, pues la infinidad de filtraciones aparecidas sobre la pareja de telÃ©fonos Galaxy ya apuntaban a un aumento.\r\n\r\nSin mÃ¡s, pasamos a ver cuÃ¡nto costarÃ¡n los Samsung Galaxy S9 y S9+ en EspaÃ±a, LatinoamÃ©rica, y el resto de regiones del mundo en las que Samsung venderÃ¡ de forma oficial sus nuevos terminales.', 'galeria/noticias/noti1.jpg', '2018-11-28 21:45:28', 'publicada'),
-(20, 'galaxy', 'Samnsung Galaxy s9', 'Los Samsung Galaxy S9 y S9+ acaban de ser presentados en el MWC 2018, y ya conocemos sus precios y la fecha en la que aterrizarÃ¡n en las tiendas. Ahora que ya hemos visto todas las caracterÃ­sticas, especificaciones y detalles de los Galaxy S9 y S9+, y lo hemos comparado con uno de los grandes rivales como lo es el iPhone X, llega el momento de ver cuÃ¡ndo y cÃ³mo se podrÃ¡ comprar este dÃºo de terminales estrella surcoreanos, llamados a ser dos de los mÃ¡s vendidos del aÃ±o dentro del segmento del a gama alta.\r\n\r\nSi bien es cierto que el precio ha aumentado con respecto a los anteriores Galaxy S8 y S8+, no es un crecimiento que nos pille por sorpresa, pues la infinidad de filtraciones aparecidas sobre la pareja de telÃ©fonos Galaxy ya apuntaban a un aumento.\r\n\r\nSin mÃ¡s, pasamos a ver cuÃ¡nto costarÃ¡n los Samsung Galaxy S9 y S9+ en EspaÃ±a, LatinoamÃ©rica, y el resto de regiones del mundo en las que Samsung venderÃ¡ de forma oficial sus nuevos terminales.', 'galeria/noticias/noti1.jpg', '2018-11-28 21:45:28', 'publicada');
+(27, 'holapinche', 'holaaaaa', 'aaaaaaaaaaa', 'galeria/noticias/noti2.jpg', '2018-11-29 05:31:54', 'Eliminada'),
+(28, 'noticia', 'noticia de prueba', 'noticia numero uno. noticia de prueba', 'galeria/noticias/noticia.png', '2018-11-29 18:51:37', 'Publicada'),
+(29, 'noticia', 'pruebaaa', 'notonqskandoknkdnkwendkwesweasfkwefopswedc', 'galeria/noticias/titaniumfix.png', '2018-11-29 18:53:49', 'Eliminada');
+
+-- --------------------------------------------------------
+
+--
+-- Estructura de tabla para la tabla `peliculas`
+--
+
+CREATE TABLE `peliculas` (
+  `ID_PELICULA` int(11) NOT NULL,
+  `PEL_TITULO` varchar(100) NOT NULL,
+  `PEL_SIPOPSIS` text NOT NULL,
+  `PEL_GENERO` text NOT NULL,
+  `PEL_IMG` varchar(200) NOT NULL,
+  `PEL_ESTADO` varchar(50) NOT NULL
+) ENGINE=MyISAM DEFAULT CHARSET=latin1;
 
 -- --------------------------------------------------------
 
@@ -74,7 +84,8 @@ CREATE TABLE `usuarios` (
 
 INSERT INTO `usuarios` (`ID_USUARIO`, `USUARIO`, `USU_CONTRASENA`, `USU_APATERNO`, `USU_AMATERNO`, `USU_NOMBRE`, `USU_CORREO`, `USU_TIPO`) VALUES
 (1, 'fer', 'c4ca4238a0b923820dcc509a6f75849b', 'Garcia', 'Garcia', 'Fernando', 'fer@.com', 'Administrador'),
-(2, NULL, NULL, 'c', 'c', 'c', 'c', 'Administrador');
+(2, NULL, NULL, 'c', 'c', 'c', 'c', 'Administrador'),
+(3, 'Admin', '81dc9bdb52d04dc20036dbd8313ed055', 'garcia', 'garcia', 'Fernando', 'fernando@gmail.com', 'Administrador');
 
 --
 -- Índices para tablas volcadas
@@ -85,6 +96,12 @@ INSERT INTO `usuarios` (`ID_USUARIO`, `USUARIO`, `USU_CONTRASENA`, `USU_APATERNO
 --
 ALTER TABLE `noticias`
   ADD PRIMARY KEY (`ID_NOTICIA`);
+
+--
+-- Indices de la tabla `peliculas`
+--
+ALTER TABLE `peliculas`
+  ADD PRIMARY KEY (`ID_PELICULA`);
 
 --
 -- Indices de la tabla `usuarios`
@@ -100,11 +117,17 @@ ALTER TABLE `usuarios`
 -- AUTO_INCREMENT de la tabla `noticias`
 --
 ALTER TABLE `noticias`
-  MODIFY `ID_NOTICIA` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
-
-
-  ALTER TABLE `usuarios`
-  MODIFY `ID_USUARIO` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=21;
+  MODIFY `ID_NOTICIA` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=30;
+--
+-- AUTO_INCREMENT de la tabla `peliculas`
+--
+ALTER TABLE `peliculas`
+  MODIFY `ID_PELICULA` int(11) NOT NULL AUTO_INCREMENT;
+--
+-- AUTO_INCREMENT de la tabla `usuarios`
+--
+ALTER TABLE `usuarios`
+  MODIFY `ID_USUARIO` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
 /*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
 /*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
